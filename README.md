69c924be23f8a595d2da9a8920835d099103448c
Tommi Palomäki <tommi.palomaki@digia.com>
2017-07-04 10:03:28 +0300

    Works with CentOS 5 now. Improved documentation.

--- README.md
+++ README.md
@@ -6,47 +6,93 @@ Build Digia Pulssi specific Zabbix Agent installation packages. The changes intr
   enable new configuration features that allow better control at the monitored host as to the files and
   logs monitored
 - Bundle monitoring scripts at https://github.com/digiapulssi/zabbix-monitoring-scripts
+- Default configuration does not define hostname but takes it from system hostname
+- Default configuration defines Timeout of 15 seconds (instead of default 3 seconds)
 
 # Download
 
 Download the latest installation packages from https://github.com/digiapulssi/zabbix-agent/releases/latest
 
+- CentOS / RedHat / Oracle Linux 5.x: zabbix-agent-VERSION.digiapulssi.el5.x86_64.rpm
+- CentOS / RedHat / Oracle Linux 6.x: zabbix-agent-VERSION.digiapulssi.el6.x86_64.rpm
+- CentOS / RedHat / Oracle Linux 7.x: zabbix-agent-VERSION.digiapulssi.el7.x86_64.rpm
+
 # Installation and Configuration
 
-Install the RPM package fitting with the following command:
+Install the RPM package with the following command:
 
 ```
-yum localinstall zabbix-agent-3.2.3.digiapulssi.elX.x86_64.rpm
-(replace X with your CentOS/RedHat/Oracle Linux version number)
+yum localinstall zabbix-agent-VERSION.digiapulssi.elVER.x86_64.rpm
 ```
 
 After installation, you should configure the following sections in /etc/zabbix/zabbix_agentd.conf file.
 Find the current configuration lines and replace them as follows:
 ```
-Server=ZABBIX_PROXY_OR_SERVER_ADDRESS
-ServerActive=ZABBIX_PROXY_OR_SERVER_ADDRESS
-Hostname=
-# TBD Check Jenni's email
+(Under Passive checks related)
+Server=ZABBIX_SERVER_SOURCE_ADDRESS1,ZABBIX_SERVER_SOURCE_ADDRESS2
 
-Optional settings:
-LogFileSize=50 (to enable automatic log rotation with 50 MB limit)
+(Under Active checks related)
+ServerActive=ZABBIX_SERVER_DEST_ADDRESS
 ```
 
-After configuration, you can start the agent:
+In elevated security level systems (Vahti korotettu) you should configure all
+the allowed monitored files under AllowedPath setting. The files configured must
+not contain sensitive (ST III) information.
+
+```
+(Under ADVANCED CONTROL OVER FILES)
+AllowedPath=PATH_TO_FILE1
+AllowedPath=PATH_TO_FILE2
+
+To allow monitoring of all files under /var/log/example/:
+AllowedPath=/var/log/example/.*
+```
+
+After configuration, you can start the agent and configure it to auto-start on boot:
 ```
 service zabbix-agent start
+chkconfig zabbix-agent on
+```
+
+# Upgade over Existing Zabbix Agent Installation
+
+In case you already have the official Zabbix Agent installed on your system,
+you should uninstall it before installing digiapulssi version (in order to
+get the latest configuration files that will not be installed if there
+is an existing version in the system).
+
+```
+yum erase zabbix-agent
 ```
 
 # Troubleshooting
 
-Zabbix agent log is located by default in /var/log/zabbix/zabbix_agentd.log
+Zabbix agent log is located by default in /var/log/zabbix/zabbix_agentd.log.
+Check for the following lines which indicate connection problems:
 
-Firewall openings can be checked with one of the following tools, depending on your system:
 ```
-10051
-telnet ZABBIX_PROXY_OR_SERVER_ADDRESS 10051
-nc -zv ZABBIX_PROXY_OR_SERVER_ADDRESS 10051
+84:20170704:065535.728 active check configuration update from [ZABBIX_SERVER_DEST_ADDRESS:10051] started to fail (cannot connect to [[ZABBIX_SERVER_DEST_ADDRESS]:10051]: [111] Connection refused
 ```
+
+Firewall openings for active checks can be checked with one of the following tools, depending on your system:
+```
+telnet ZABBIX_SERVER_DEST_ADDRESS 10051
+nc -zv ZABBIX_SERVER_DEST_ADDRESS 10051
+
+# Without any additional tools (from bash):
+# The command should run 1-2 seconds and then exit with status code 0.
+# If firewall openings are not ok the command runs 1-2 minutes and then prints Connection timed out.
+cat < /dev/tcp/ZABBIX_SERVER_DEST_ADDRESS/10051; echo $?
+```
+
+# Installation of Custom Monitoring Scripts
+
+Sometimes you need to install custom monitoring scripts to your host.
+A custom monitoring script consists of a script file and a configuration files.
+Install the files to the following locations:
+
+- Script file: /etc/zabbix/scripts
+- Configuration file (something.conf): /etc/zabbix/zabbix-agentd.d/
 
 # How to Release a New Version (for Digia Pulssi Developers)
 
@@ -58,11 +104,14 @@ docker build -t zabbix-rpm:centos7 -f Dockerfile.centos7 .
 
 Then run the following commands to produce new installation packages for different platforms:
 
-docker run --rm -v $(pwd)/RPMS:/root/rpmbuild/RPMS zabbix-rpm:centos5
+docker run --rm -v $(pwd)/RPMS:/usr/src/redhat/RPMS zabbix-rpm:centos5
 docker run --rm -v $(pwd)/RPMS:/root/rpmbuild/RPMS zabbix-rpm:centos6
 docker run --rm -v $(pwd)/RPMS:/root/rpmbuild/RPMS zabbix-rpm:centos7
 
 Finally, copy the zabbix-agent rpm packages from RPMS folder to Github releases section.
+
+- Note that CentOS 5 package name must be modified to include "el5" tag similarly to the others
+- Remove "centos" from CentOS 7 package name
 
 # Implementation Notes
 
--- build.sh
+++ build.sh
@@ -7,16 +7,22 @@ rpm -ih zabbix-*.src.rpm
 
 # Get latest sources from Pulssi repository, including Pulssi changes
 wget -nv https://github.com/digiapulssi/zabbix/tarball/branch-3.2.3
-mkdir zabbix-3.2.3.digiapulssi
-tar zxf branch-3.2.3 -C zabbix-3.2.3.digiapulssi --strip 1
+mkdir zabbix-3.2.3
+tar zxf branch-3.2.3 -C zabbix-3.2.3 --strip 1
+pushd zabbix-3.2.3
+
+# Default configuration changes
+# Do not specify Hostname but use system hostname by default
+sed -i '/^Hostname=Zabbix server/d' conf/zabbix_agentd.conf
+# Bigger timeout
+sed -i '/^# Timeout=3/a Timeout=15' conf/zabbix_agentd.conf
 
 # Compile tarball so that it's identical to the one included in official SPRM
 # See: https://www.zabbix.org/wiki/Compilation_instructions
-pushd zabbix-3.2.3.digiapulssi
-sed -i 's/3\.2\.3/3.2.3.digiapulssi/g' configure.ac
+#sed -i 's/3\.2\.3/3.2.3.digiapulssi/g' configure.ac
 
 # file paths are so close to 99 long that adding digiapulssi to version number makes them too long with old tar version
-sed -i 's/AM_INIT_AUTOMAKE/AM_INIT_AUTOMAKE([1.9 tar-ustar])/g' configure.ac
+#sed -i 's/AM_INIT_AUTOMAKE/AM_INIT_AUTOMAKE([1.9 tar-ustar])/g' configure.ac
 ./bootstrap.sh
 ./configure
 make dbschema
@@ -24,62 +30,61 @@ make css
 make gettext
 mkdir src/zabbix_java/bin # this is some workaround documented nowhere but necessary for make dist to work...
 make dist
-mkdir -p /root/rpmbuild/SOURCES # required for CentOS 5 which doesn't have fedora-packager
-mv zabbix-3.2.3.digiapulssi.tar.gz /root/rpmbuild/SOURCES/
+mkdir -p $RPMBUILD/SOURCES # required for CentOS 5 which doesn't have fedora-packager
+rm $RPMBUILD/SOURCES/zabbix-3.2.3.tar.gz # Original one installed by SRPM
+mv zabbix-3.2.3.tar.gz $RPMBUILD/SOURCES/
 popd
-
-# Remove the original source package
-rm -f /root/rpmbuild/SOURCES/zabbix-3.2.3.tar.gz
 
 # Get Pulssi monitoring scripts
 wget https://github.com/digiapulssi/zabbix-monitoring-scripts/tarball/master
 tar -zxvf master */scripts --strip 1
 tar -zxvf master */config --strip 1
-tar cvf /root/rpmbuild/SOURCES/scripts.tar.gz scripts
+tar cvf $RPMBUILD/SOURCES/scripts.tar.gz scripts
 pushd config
-tar cvf /root/rpmbuild/SOURCES/scripts_config.tar.gz .
+tar cvf $RPMBUILD/SOURCES/scripts_config.tar.gz .
 popd
 
 # Update SPEC contents
-sed -i 's/^Version:\s\+3.2.3/Version:        3.2.3.digiapulssi/' /root/rpmbuild/SPECS/zabbix.spec
+#sed -i 's/^Version:\s\+3.2.3/Version:        3.2.3.digiapulssi/' $RPMBUILD/SPECS/zabbix.spec
+sed -i 's/^\(Release:\s\+.*\)$/\1.digiapulssi/' $RPMBUILD/SPECS/zabbix.spec
 
 ##############################################################33
 # Monitoring scripts under /etc/zabbix/scripts
 
-sed -i '/^Source15/a Source16:		scripts.tar.gz' /root/rpmbuild/SPECS/zabbix.spec
-sed -i '/^%prep/a %setup -T -b 16 -q -n scripts' /root/rpmbuild/SPECS/zabbix.spec
+sed -i '/^Source15/a Source16:		scripts.tar.gz' $RPMBUILD/SPECS/zabbix.spec
+sed -i '/^%prep/a %setup -T -b 16 -q -n scripts' $RPMBUILD/SPECS/zabbix.spec
 
 # install section
 sed -i '/^%clean/i # install monitoring scripts \
 cp -r ../scripts $RPM_BUILD_ROOT%{_sysconfdir}/zabbix/ \
 chmod 0755 $RPM_BUILD_ROOT%{_sysconfdir}/zabbix/scripts/* \
-' /root/rpmbuild/SPECS/zabbix.spec
+' $RPMBUILD/SPECS/zabbix.spec
 
 # %files agent section
-sed -i '/^%dir %{_sysconfdir}\/zabbix\/zabbix_agentd.d/i %dir %{_sysconfdir}/zabbix/scripts' /root/rpmbuild/SPECS/zabbix.spec
+sed -i '/^%dir %{_sysconfdir}\/zabbix\/zabbix_agentd.d/i %dir %{_sysconfdir}/zabbix/scripts' $RPMBUILD/SPECS/zabbix.spec
 # Add each script file individually to files section
 for scriptpath in scripts/*; do
    scriptfile=$(basename $scriptpath)
-   sed -i '/^%dir %{_sysconfdir}\/zabbix\/scripts/a %config(noreplace) %{_sysconfdir}/zabbix/scripts/'${scriptfile} /root/rpmbuild/SPECS/zabbix.spec
+   sed -i '/^%dir %{_sysconfdir}\/zabbix\/scripts/a %config(noreplace) %{_sysconfdir}/zabbix/scripts/'${scriptfile} $RPMBUILD/SPECS/zabbix.spec
 done
 
 ##############################################################33
 # Monitoring script configuration files under /etc/zabbix/zabbix_agentd.d
 
-sed -i '/^Source16/a Source17:		scripts_config.tar.gz' /root/rpmbuild/SPECS/zabbix.spec
-sed -i '/^%setup -T -b 16/a %setup -T -a 17 -q -c -n zabbix_agentd.d' /root/rpmbuild/SPECS/zabbix.spec
+sed -i '/^Source16/a Source17:		scripts_config.tar.gz' $RPMBUILD/SPECS/zabbix.spec
+sed -i '/^%setup -T -b 16/a %setup -T -a 17 -q -c -n zabbix_agentd.d' $RPMBUILD/SPECS/zabbix.spec
 
 # install section
 sed -i '/^%clean/i # install monitoring script configuration files \
 cp ../zabbix_agentd.d/* $RPM_BUILD_ROOT%{_sysconfdir}/zabbix/zabbix_agentd.d/ \
 chmod 0644 $RPM_BUILD_ROOT%{_sysconfdir}/zabbix/zabbix_agentd.d/* \
-' /root/rpmbuild/SPECS/zabbix.spec
+' $RPMBUILD/SPECS/zabbix.spec
 
 # %files agent section
 # Add each configuration file individually to files section
 for confpath in config/*; do
    conffile=$(basename $confpath)
-   sed -i '/^%dir %{_sysconfdir}\/zabbix\/zabbix_agentd.d/a %config(noreplace) %{_sysconfdir}/zabbix/zabbix_agentd.d/'${conffile} /root/rpmbuild/SPECS/zabbix.spec
+   sed -i '/^%dir %{_sysconfdir}\/zabbix\/zabbix_agentd.d/a %config(noreplace) %{_sysconfdir}/zabbix/zabbix_agentd.d/'${conffile} $RPMBUILD/SPECS/zabbix.spec
 done
 
 # Disable python build by rpmbuild (taken from https://www.redhat.com/archives/rpm-list/2007-November/msg00020.html)
@@ -87,5 +92,9 @@ done
 sed -i '/^#!\/bin\/bash/a exit 0' /usr/lib/rpm/brp-python-bytecompile
 
 # Build the rpm package
-# TBD: Fall back to rpmbuild with CentOS 5, see https://stackoverflow.com/questions/592620/check-if-a-program-exists-from-a-bash-script
-rpmbuild-md5 -bb /root/rpmbuild/SPECS/zabbix.spec
+if type rpmbuild-md5 >/dev/null 2>&1; then
+   rpmbuild-md5 -bb $RPMBUILD/SPECS/zabbix.spec
+else
+   # CentOS 5 doesn't have fedora-package which would include rpmbuild-md5
+   rpmbuild -bb $RPMBUILD/SPECS/zabbix.spec
+fi
